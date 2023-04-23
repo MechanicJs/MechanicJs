33df69091ef17f836d18a9163a0d5d0615eb1e5f
gilmujjang <40172373+gilmujjang@users.noreply.github.com>
2023-04-23 22:41:12 +0900

    [main] chore: Prettier & EsLint setup
    prettier & eslint setup

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	printWidth: 80,
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	trailingComma: 'none',
+	jsxBracketSameLine: true,
+	bracketSpacing: true,
+	arrowParens: 'always'
+};
