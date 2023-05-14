d22008a5b3d1dd945fef2369648b6500c3b1d82d
gilmujjang <40172373+gilmujjang@users.noreply.github.com>
2023-05-14 20:04:30 +0900

    [Test] average 테스트코드 작성
    average() 테스트 코드 작성

--- test/numbers.test.js
+++ test/numbers.test.js
@@ -1,4 +1,4 @@
-const sqrt = require('../lib/index.js');
+const { sqrt, average } = require('../lib/index.js');
 
 test('square root test: sqrt(4) = 2', () => {
 	expect(sqrt(4)).toBe(2);
@@ -6,4 +6,16 @@ test('square root test: sqrt(4) = 2', () => {
 
 test('square root test: sqrt(-4) = 2i', () => {
 	expect(sqrt(-4)).toBe('2i');
+});
+
+test('average test: average([-1,-2,-3,4,5]) = 0.6', () => {
+	expect(average([-1, -2, -3, 4, 5])).toBe(0.6);
+});
+
+test('average test: average([]) = 0', () => {
+	expect(average([])).toBe(0);
+});
+
+test('average test: average([1,2,3,a]) = 0', () => {
+	expect(average([1, 2, 3, 'a'])).toBe(0);
 });
